535ca41f66ee046974e676258e66d6d84c6d3611
Phillip Truong <ptruong@konradgroup.com>
2022-07-28 13:38:18 -0400

    exercise 3

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,28 +1,38 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import styled from "styled-components/macro";
 
-import { WEIGHTS } from '../../constants';
+import { QUERIES, WEIGHTS } from "../../constants";
 
-import Breadcrumbs from '../Breadcrumbs';
-import Select from '../Select';
-import Spacer from '../Spacer';
-import ShoeSidebar from '../ShoeSidebar';
-import ShoeGrid from '../ShoeGrid';
+import Breadcrumbs from "../Breadcrumbs";
+import Select from "../Select";
+import ShoeGrid from "../ShoeGrid";
+import ShoeSidebar from "../ShoeSidebar";
+import Spacer from "../Spacer";
 
 const ShoeIndex = ({ sortId, setSortId }) => {
   return (
     <Wrapper>
       <MainColumn>
         <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
+          <Container>
+            <BreadCrumbsTitle>
+              <Breadcrumbs>
+                <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+                <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+                <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
+              </Breadcrumbs>
+            </BreadCrumbsTitle>
+            <Title>Running</Title>
+          </Container>
+          <CustomSelect>
+            <Select
+              label="Sort"
+              value={sortId}
+              onChange={(ev) => setSortId(ev.target.value)}
+            >
+              <option value="newest">Newest Releases</option>
+              <option value="price">Price</option>
+            </Select>
+          </CustomSelect>
         </Header>
         <Spacer size={32} />
         <ShoeGrid />
@@ -31,9 +41,7 @@ const ShoeIndex = ({ sortId, setSortId }) => {
         <Breadcrumbs>
           <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
           <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
         </Breadcrumbs>
         <Spacer size={42} />
         <ShoeSidebar />
@@ -49,8 +57,28 @@ const Wrapper = styled.div`
   gap: 32px;
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media ${QUERIES.laptopAndUp} {
+    flex-direction: row;
+  }
+`;
+
+const BreadCrumbsTitle = styled.div`
+  display: block;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
+`;
+
 const LeftColumn = styled.div`
   flex-basis: 248px;
+  display: none;
+  @media ${QUERIES.laptopAndUp} {
+    display: revert;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -66,6 +94,14 @@ const Header = styled.header`
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
+`;
+
+const CustomSelect = styled.div`
+  display: none;
+
+  @media ${QUERIES.tabletAndUp} {
+    display: flex;
+  }
 `;
 
 export default ShoeIndex;
